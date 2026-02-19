1d99d389b71af59927e78c4aa4faea98908fefaa
nits68 <nits.laszlo@jedlik.eu>
2026-02-19 09:10:35 +0100

    start

---
+++ app/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { clsx } from "clsx";
+import dayjs from "dayjs";
+import { SunMoon } from "lucide-react";
+import Image from "next/image";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+import { useGlobalStore } from "@/store/globalStore";
+
+export default function HomePage() {
+  // Using Zustand global store for state management example
+  const { loggedUser, setLoggedUser } = useGlobalStore();
+  const { lightTheme, setLightTheme } = useGlobalStore();
+
+  useEffect(() => {
+    toast.success(`Render on: ${dayjs().format("YYYY.MM.DD HH:mm:ss")}`);
+  }); // no dependency array to demonstrate re-render toast
+
+  function handleThemeToggle() {
+    setLightTheme(!lightTheme);
+    document.documentElement.classList.toggle("dark", lightTheme);
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-200 py-2 dark:bg-gray-800">
+      <h1 className={clsx("mb-6 text-3xl font-bold", lightTheme ? "text-black" : "text-white")}>
+        {"Hello, "}
+        {loggedUser || (
+          <Image
+            alt="next logo"
+            className="p-2 inline dark:bg-white dark:rounded-md"
+            height={0}
+            src="next.svg"
+            width={110}
+          />
+        )}
+        ! 😎
+      </h1>
+      <input
+        className="input input-primary"
+        type="text"
+        value={loggedUser || ""}
+        onChange={(e) => setLoggedUser(e.target.value)}
+      />
+      <button className="btn mt-4 btn-primary" onClick={handleThemeToggle}>
+        <SunMoon className="mr-2" size={24} />
+        Toggle Theme
+      </button>
+    </div>
+  );
+}
